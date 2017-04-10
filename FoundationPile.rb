ff263decee4235034b8414861f7244fa9e400c43
Brandon Tuttle <brandon@Brandons-MBP.localdomain>
2017-04-10 10:47:36 -0400

    Revert "Attempted to fix mouse leaving card"
    This reverts commit fd754e57e163be90304778248fb6792f76498316.

--- FoundationPile.rb
+++ FoundationPile.rb
@@ -9,38 +9,32 @@ class FoundationPile < Gtk::Fixed
     super()
     @cardsInPile = Array.new
     @currentSequence = 0
+    
     @cardSequence = Array.new
     @sequenceLabels = Array.new
-    @finished = false
-
-
-    # to set up card hints set up underneath foundation
+    
     i = order - 1
     pos = 0
     until i == 12 do
-      @cardSequence.push(i + 1) # card sequence for foundation
-
-      # creates individual label for each hint
+      @cardSequence.push(i + 1)
+      
       label = Gtk::Label.new
       label.set_markup(self.valueToString(i + 1))
       label.set_justify(Gtk::Justification::CENTER)
-
-      # creates (x,y) for each label
+      
       x = (pos * 14.4) % 72
       y = ((pos * 14.4) / 72).floor * 14.4
       if i == 9
         x -= 5
       end
       self.put(label, x + 2, y + 100)
-      @sequenceLabels.push(label) # card sequence for hints
-
-      # increments counters and position
+      @sequenceLabels.push(label)
+      
       i += order
       i = i % 13
       pos += 1
     end
-
-    # creates and pushes label and sequence for the value K
+    
     @cardSequence.push(12 + 1)
       
     label = Gtk::Label.new
@@ -57,11 +51,12 @@ class FoundationPile < Gtk::Fixed
   
   def addCard card
     nextCardInSequence = @cardSequence.first;
-
-    # determines if card belongs in the foundation
+    puts "nextCardInSequence = " + nextCardInSequence.to_s
     if card.get_value == nextCardInSequence
-
       label = @sequenceLabels[@currentSequence]
+      # puts @currentSequence
+      # puts @sequenceLabels
+      # puts @label
       label.set_markup("<span foreground=\"red\">" + self.valueToString(nextCardInSequence) + "</span>")
       
       @cardSequence[0] = nil
@@ -70,17 +65,11 @@ class FoundationPile < Gtk::Fixed
       card.flip_face_up
       self.put(card, 0, 0)
       @currentSequence += 1
-
-      # determines if foundation is finished
-      if card.get_value == 13
-        @finished = true
-      end
       return true
     end
     false
   end
-
-  # returns string value
+  
   def valueToString value
     case value
       when 1
@@ -94,15 +83,5 @@ class FoundationPile < Gtk::Fixed
       else
         return value.to_s
     end
-  end
-
-  # returns status of foundation
-  def isFinished?
-    return @finished
-  end
-
-  # returns what is next in the foundation
-  def whatsNext?
-    return @cardSequence.first
   end
 end
